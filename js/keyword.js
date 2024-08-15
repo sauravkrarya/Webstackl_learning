2fa4307cb2cb9f10c0eb8fdf4ba08e90d1232cae
Saurav kumar Arya <92774037+sauravkrarya@users.noreply.github.com>
2024-08-15 23:55:39 +0530

    u

---
+++ js/keyword.js
@@ -0,0 +1,12 @@
+if(true){
+    let a=12;
+    var b=13;
+    const c=14;
+    // document.write(a);
+    // document.write(b);
+    // document.write(c);
+}
+document.write(a);
+document.write(b);
+document.write(c);
+
--- js/sum.js
+++ js/sum.js
@@ -1,6 +1,8 @@
-let sum="23";
-let SUM="2";
-let Sum="7";
-document.write=("sum:="+sum);
-document.write=("<br/> SUM:="+SUM);
-document.write=("<br/> Sum:="+Sum);
+//javascript is case sensative so every word has different meaning if there any change in letter , it may be in capital or small.
+
+let ABC=6356;
+const aBC=343;
+const abC=1;
+const Abc=1;
+// let sum=ABC+aBC+Abc+abC;
+document.write("sum of these all number: "+(ABC+aBC+Abc+abC));
